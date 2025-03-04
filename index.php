a8743a69cd95842e8beb90887c5b82f16af628aa
rafandco <rafandpufff@gmail.com>
2025-03-04 19:58:54 +0100

    Añadir web básica

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Bienvenidos a nuestra tienda de libros</h1>
+    <p>Encuentra los mejores libros a los mejores precios.</p>
+    <h2>Como sabemos que te gusta</h2>
+    <ul>
+      <li>Libros físicos</li>
+      <li>Ficción</li>
+    </ul>
+    <h3>Te recomendamos:</h3>
+    <h2>1984</h2>
+    <img
+      width="250px"
+      src="https://www.popularlibros.com/imagenes-webp-grandes/9780141/978014103614.webp"
+      alt="Foto de un libro"
+    />
+  </body>
+</html>
